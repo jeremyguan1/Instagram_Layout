46e160fc451e48160f153c6b641bef9f3807e42b
Jeremy Guan <jeremyguan1@gmail.com>
2020-01-02 17:45:08 -0800

    setup with postcss and gulp

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var postcss = require("gulp-postcss");
+var gulp = require("gulp");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
+var sass = require("gulp-sass");
+
+gulp.task("sass", function() {
+  var plugins = [autoprefixer({ browsers: ["last 1 version"] }), cssnano()];
+  return gulp
+    .src("index.scss")
+    .pipe(sass())
+    .pipe(postcss(plugins))
+    .pipe(gulp.dest("./dest"));
+});
+
+gulp.task("watch:sass", function() {
+  gulp.watch("**/*.scss", gulp.series("sass"));
+});
